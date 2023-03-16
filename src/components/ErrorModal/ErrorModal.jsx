1f11587249fc4ed79ddd51a3c511e2c74f908eba
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-03-16 19:13:03 +0100

    send gmail form & modal

---
+++ src/components/ErrorModal/ErrorModal.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import classes from "./ErrorModal.module.css";
+const ErrorModal = ({ children, style, title, closeModal }) => {
+  //   return (
+  //     <>
+  //       <div className={classes.modal}>
+  //         <span className={classes.title}>{children}</span>
+  //         <p>{title}</p>
+  //         <div style={style} className={classes.button} onClick={closeModal}>
+  //           OK
+  //         </div>
+  //       </div>
+  //     </>
+  //   );
+};
+
+export default ErrorModal;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,34 +1,46 @@
 import classes from "./Form.module.css";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
-const Form = () => {
+import { Oval } from "react-loader-spinner";
+import ErrorModal from "../ErrorModal/ErrorModal";
+let className;
+const Form = (props) => {
   const form = useRef();
+  const [loader, setLoader] = useState(false);
+  const [successful, setSuccessful] = useState(false);
+  const [modal, setModal] = useState(false);
 
-  const sendEmail = (e) => {
+  function closeModal() {
+    setModal(false);
+  }
+
+  const sendEmail = async (e) => {
     e.preventDefault();
-
-    emailjs
-      .sendForm(
+    setModal(true);
+    setLoader(true);
+    try {
+      await emailjs.sendForm(
         "service_3ca2cy5",
         "template_db52oq4",
         form.current,
-        "6qGSj8MWgwS5D-RL1"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
+        "6qGSj8MWgwS5D-RL"
       );
+      className = "green";
+      setSuccessful("Thank you for contacting me");
+      setLoader(false);
+    } catch (error) {
+      setSuccessful("Something went wrong, please try again");
+      setLoader(false);
+    }
   };
+
   return (
     <form ref={form} onSubmit={sendEmail} className={classes.form}>
       <div className={classes.inputWrapper}>
         <input type="text" id="name" name="user_name" required />
         <label htmlFor="user_name">Your Name</label>
       </div>
+
       <div className={classes.inputWrapper}>
         <input type="text" id="email" name="user_email" required />
         <label htmlFor="user_email">Your email</label>
@@ -40,6 +52,30 @@ const Form = () => {
       <div className={classes.wrapperButton}>
         <button>SEND</button>
       </div>
+      {modal && (
+        <ErrorModal
+          title={successful}
+          style={{
+            backgroundColor: className === "green" ? "green" : "red",
+          }}
+          closeModal={closeModal}
+        >
+          {loader && (
+            <Oval
+              height={80}
+              width={80}
+              color="#4fa94d"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+              ariaLabel="oval-loading"
+              secondaryColor="#4fa94d"
+              strokeWidth={2}
+              strokeWidthSecondary={2}
+            />
+          )}
+        </ErrorModal>
+      )}
     </form>
   );
 };
