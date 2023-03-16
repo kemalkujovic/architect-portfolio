744c698d77534a7957c5c5260a86b27f8480b73c
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-03-16 02:09:34 +0100

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,37 @@
-import React from "react";
 import classes from "./Form.module.css";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_3ca2cy5",
+        "template_db52oq4",
+        form.current,
+        "6qGSj8MWgwS5D-RL1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <form className={classes.form}>
+    <form ref={form} onSubmit={sendEmail} className={classes.form}>
       <div className={classes.inputWrapper}>
-        <input type="text" id="name" name="name" required />
-        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" name="user_name" required />
+        <label htmlFor="user_name">Your Name</label>
       </div>
       <div className={classes.inputWrapper}>
-        <input type="text" id="email" name="email" required />
-        <label htmlFor="email">Your email</label>
+        <input type="text" id="email" name="user_email" required />
+        <label htmlFor="user_email">Your email</label>
       </div>
       <div className={classes.inputWrapper}>
         <input type="text" id="message" name="message" required />
