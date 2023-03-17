577faffcbe3e1cfa46cb2832f414c150599bf063
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-03-17 02:56:39 +0100

    add main page background

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import Contact from "../Contact/Contact";
+import classes from "./HomePage.module.css";
+import Footer from "../../components/Footer/Footer";
 const HomePage = () => {
-  return <Contact></Contact>;
+  return (
+    <>
+      <div>
+        <div className={classes.backgroundContainer}>
+          <div className={classes.imageWrapper}>
+            <img
+              src="https://static.wixstatic.com/media/2b854b_b6567a99ad734a20b4c2a3227024f9e5~mv2.jpg/v1/fill/w_1743,h_687,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2b854b_b6567a99ad734a20b4c2a3227024f9e5~mv2.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default HomePage;
