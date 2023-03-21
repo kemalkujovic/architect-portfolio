87bc894baab44debbc000dbe370e3fefed0b6e84
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-03-21 18:08:37 +0100

    add fonts

--- src/App.js
+++ src/App.js
@@ -5,7 +5,29 @@ import Projects from "./pages/Projects/Projects";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import HomePage from "./pages/Home/HomePage";
-import axios from "axios";
+import "./assets/fonts/Lato-Black.ttf";
+import "./assets/fonts/Lato-BlackItalic.ttf";
+import "./assets/fonts/Lato-Bold.ttf";
+import "./assets/fonts/Lato-BoldItalic.ttf";
+import "./assets/fonts/Lato-Italic.ttf";
+import "./assets/fonts/Lato-Light.ttf";
+import "./assets/fonts/Lato-LightItalic.ttf";
+import "./assets/fonts/Lato-Regular.ttf";
+import "./assets/fonts/Lato-Thin.ttf";
+import "./assets/fonts/Lato-ThinItalic.ttf";
+import "./assets/fonts/Montserrat-Black.ttf";
+import "./assets/fonts/Montserrat-BlackItalic.ttf";
+import "./assets/fonts/Montserrat-Bold.ttf";
+import "./assets/fonts/Montserrat-BoldItalic.ttf";
+import "./assets/fonts/Montserrat-ExtraBold.ttf";
+import "./assets/fonts/Montserrat-ExtraBoldItalic.ttf";
+import "./assets/fonts/Montserrat-ExtraLight.ttf";
+import "./assets/fonts/Montserrat-ExtraLightItalic.ttf";
+import "./assets/fonts/Montserrat-Italic-VariableFont_wght.ttf";
+import "./assets/fonts/Montserrat-Italic.ttf";
+import "./assets/fonts/Montserrat-Light.ttf";
+import "./assets/fonts/Montserrat-Regular.ttf";
+
 function App() {
   const router = createBrowserRouter([
     {
