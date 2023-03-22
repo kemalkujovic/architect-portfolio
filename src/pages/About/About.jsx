f10a6fe08960a9e55f6f274c7dc5fd4030f5c001
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-03-22 04:35:46 +0100

    refactoring code

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Footer from "../../components/Footer/Footer";
+import classes from "./About.module.css";
 const About = () => {
-  return <p>About page</p>;
+  return (
+    <>
+      <div className={classes["page-container"]}>
+        <p>About Page</p>
+        <Footer></Footer>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- src/pages/Projects/ProjectsDetail.jsx
+++ src/pages/Projects/ProjectsDetail.jsx
@@ -3,31 +3,35 @@ import { data } from "../../components/Project/ProjectsHomePage";
 import classes from "./ProjectsDetail.module.css";
 import { Grid } from "@mui/material";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import Footer from "../../components/Footer/Footer";
 function ProjectsDetail() {
   const params = useParams();
   const filteredImage = data.filter((el) => el.title === params.projectsId);
   return (
-    <div className={classes.projectsImageContainer}>
-      <Grid item spacing={3} container>
-        {filteredImage[0].subImage.map((el, index) => {
-          return (
-            <Grid key={index} item lg={3}>
-              <div className={classes.imageWrapper}>
-                <LazyLoadImage
-                  key={index}
-                  src={el}
-                  effect="blur"
-                  alt="Project"
-                  style={{
-                    transition: "all 1s ease",
-                  }}
-                />
-              </div>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </div>
+    <>
+      <div className={classes.projectsImageContainer}>
+        <Grid item spacing={3} container>
+          {filteredImage[0].subImage.map((el, index) => {
+            return (
+              <Grid key={index} item lg={3}>
+                <div className={classes.imageWrapper}>
+                  <LazyLoadImage
+                    key={index}
+                    src={el}
+                    effect="blur"
+                    alt="Project"
+                    style={{
+                      transition: "all 1s ease",
+                    }}
+                  />
+                </div>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </div>
+      <Footer />
+    </>
   );
 }
 
